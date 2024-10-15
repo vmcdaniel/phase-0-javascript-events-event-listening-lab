0bc011c19e4e9166276baa95d0335140e3b4efbb
Victoria <vmcdaniel459@gmail.com>
2024-10-15 17:26:32 -0400

    good job vic

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function addingEventListener() {const input = document.getElementById('button');
     input.addEventListener('click', function() {
-    alert('I was clicked!');
+        alert('I was clicked!');
     });
 }
+
+
